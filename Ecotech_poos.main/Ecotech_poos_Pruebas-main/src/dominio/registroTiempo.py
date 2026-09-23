f1ea545b558284e195ccedec22981769745b33e8
Jean Carlo Yauri Pastén - INACAP <jean.yauri@inacapmail.cl>
2026-09-23 11:00:24 -0300

    Delete Ecotech_poos.main/Ecotech_poos_Pruebas-main directory

--- Ecotech_poos.main/Ecotech_poos_Pruebas-main/src/dominio/registroTiempo.py
+++
@@ -1,7 +0,0 @@
-class RegistroTiempo:
-    def __init__(self, fecha: str, hora: str):
-        self.fecha = fecha
-        self.hora = hora
-        
-    def mostrar_registro(self) -> str:
-        return f"fecha: {self.fecha} hrs: {self.hora}"
