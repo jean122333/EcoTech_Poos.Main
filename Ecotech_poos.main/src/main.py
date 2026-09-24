79fb48ccd8ef5568d95406a673b4116a638b7eec
Joaquin Astorga <joaquin.astorga05@inacapmail.cl>
2026-09-24 01:34:46 -0300

    agregue en departamento @property para poder encapsular ademas del agregado de gerente e id_departamento y que sea void poniendo "None"

--- Ecotech_poos.main/src/main.py
+++ Ecotech_poos.main/src/main.py
@@ -1,4 +1,5 @@
 # main.py
+from dominio.departamento import Departamento
 from persistencia.crear_bd import crear_tablas
 from dominio.empleado import Empleado
 from persistencia.empleado_dao import EmpleadoDAO
@@ -14,12 +15,24 @@ empleado = Empleado(
 EmpleadoDAO.insertar(empleado)
 print("Insertado:", empleado.mostrar_datos())
 
-encontrado = EmpleadoDAO.buscar_por_id(empleado.id)
+encontrado = EmpleadoDAO.buscar_por_id(empleado.id_empleado)
 print("Encontrado:", encontrado)
 
 print("Listado:")
 for item in EmpleadoDAO.listar():
     print(item)
+
+departamento = Departamento("Ventas", 1)
+departamento.agregar_empleado(empleado)
+print("Departamento:", empleado.departamento.nombre)
+print("Empleados en el departamento:", departamento.cantidad_empleados())
+ 
+
+empleado.registrar_horas("2026-09-24", 8)
+empleado.registrar_horas("2026-09-25", 6)
+print("Horas registradas:")
+for registro in empleado.registros:
+    print(registro.mostrar_registro())
     
-    
+  
     
