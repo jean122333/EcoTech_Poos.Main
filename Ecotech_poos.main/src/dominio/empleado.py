1ae0a818efc40f7d9ac5cd09a2d33629f5763a33
Joaquin Astorga <joaquin.astorga05@inacapmail.cl>
2026-09-23 23:50:59 -0300

    agregue salario id_empleado, get_salario() y registrar_horas y los atributos privados con @property

--- Ecotech_poos.main/src/dominio/empleado.py
+++ Ecotech_poos.main/src/dominio/empleado.py
@@ -2,13 +2,46 @@
 from dominio.registroTiempo import RegistroTiempo
 
 class Empleado:
-    def __init__(self, nombre, correo, id=None):
-        self.id = id
-        self.nombre = nombre
-        self.correo = correo
-        
+    def __init__(self, nombre, correo, id_empleado=None, salario=0.0, departamento=None):
+        self._id_empleado = id_empleado
+        self._nombre = nombre
+        self._correo = correo
+        self._salario = salario
+        self._departamento = departamento
+        self._registros = []
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @property
+    def correo(self):
+        return self._correo
+
+    @property
+    def id_empleado(self):
+        return self._id_empleado
+
+    @property
+    def salario(self):
+        return self._salario
+
+    @property
+    def departamento(self):
+        return self._departamento
+
+    def get_salario(self):
+        return self._salario
+
+    def registrar_horas(self, fecha, horas) -> bool:
+        if horas <= 0 or horas > 24:
+            return False
+        registro = RegistroTiempo(fecha, horas)
+        self._registros.append(registro)
+        return True
+
     def __str__(self):
-        return f"Empleado(id={self.id}, nombre={self.nombre}, correo={self.correo})"
+        return f"Empleado(id={self.id_empleado}, nombre={self.nombre}, correo={self.correo})"
 
     def mostrar_datos(self) -> str:
-        return f"id:{self.id} {self.nombre} - {self.correo}"
+        return f"id:{self.id_empleado} {self.nombre} - {self.correo}"
